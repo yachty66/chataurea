d750596885efc67731e93d6a18a481c9771059a9
deeplearning <maxhager28@gmail.com>
2025-04-05 10:41:41 +0200

    chat

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
   return (
     <div className="flex flex-col h-screen bg-white">
       {/* Chat messages area */}
-      <div className="flex-1 overflow-y-auto p-4 space-y-4 relative">
+      <div className="flex-1 overflow-y-auto p-4 relative flex flex-col justify-end">
         {/* Logo in the middle */}
         {messages.length === 0 && (
           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
@@ -39,22 +39,25 @@ export default function Home() {
           </div>
         )}
         
-        {messages.map((message, index) => (
-          <div
-            key={index}
-            className={`flex ${message.isUser ? 'justify-end' : 'justify-start'}`}
-          >
+        {/* Centered container for messages */}
+        <div className="max-w-3xl mx-auto w-full space-y-4">
+          {messages.map((message, index) => (
             <div
-              className={`max-w-[70%] rounded-lg p-4 ${
-                message.isUser
-                  ? 'bg-black text-white'
-                  : 'bg-gray-100 text-black border border-gray-200'
-              }`}
+              key={index}
+              className={`flex ${message.isUser ? 'justify-end' : 'justify-start'}`}
             >
-              {message.text}
+              <div
+                className={`max-w-[70%] rounded-lg p-4 ${
+                  message.isUser
+                    ? 'bg-black text-white'
+                    : 'bg-gray-100 text-black border border-gray-200'
+                }`}
+              >
+                {message.text}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
 
       {/* Message input area */}
